eaa537ca84b08d5474e5e3c60084413cf77c2297
zhusc6666-blip <zhusc6666@gmail.com>
2026-03-16 23:18:42 +0800

    Update chapter01.py

--- chapter01.py
+++ chapter01.py
@@ -1,2 +1,20 @@
 print("hello world!")
-print ("ddddd")
+print ("ddddd")# print("i dont want to learn coding, its so boring !!!!!!!!!!!!")
+
+print("一共是:",100-25*3%4) 
+print(75%4)
+
+my_name = 'ivy'
+my_age = 21
+my_height = 74.0 #inch
+my_weight = 180.0 #磅
+
+print(f"我叫{my_name}")
+print(f"我今年{my_age}")
+print(f"我体重是{my_height}")
+print(f"将我身高换算成厘米:{my_height*2.54}")
+print(f"将我体重换算成公斤: {my_weight*0.4536}")
+
+# 四舍五入函数：round()
+print(round(1.76666))
+
